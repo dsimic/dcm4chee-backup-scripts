3511713ae4d2f5d9abca2b738c033140a79be0e9
dsimic <simic4@gmail.com>
2016-08-16 11:49:12 -0500

    added config_example

--- common.py
+++ common.py
@@ -3,6 +3,13 @@ import os
 BASEDIR = os.path.dirname(os.path.abspath(os.path.realpath(__file__)))
 DEF_CONFIG_PATH = os.path.join(BASEDIR, 'config_local.py')
 
+if not os.path.exists(DEF_CONFIG_PATH):
+    """
+    For quick start set to config_example if user did not yet
+    setup config_local.py
+    """
+    DEF_CONFIG_PATH = os.path.join(BASEDIR, 'config_example.py')
+
 config = None
 
 
